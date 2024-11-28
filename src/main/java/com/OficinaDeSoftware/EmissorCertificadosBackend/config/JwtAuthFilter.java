797cfd5609ffd074210878ed9143cbc22d18a773
luizh3 <luiz7355608@gmail.com>
2024-11-28 17:50:22 -0300

    feat: #CERUTF-0 Ajustar erro do filtro das rotas

--- src/main/java/com/OficinaDeSoftware/EmissorCertificadosBackend/config/JwtAuthFilter.java
+++ src/main/java/com/OficinaDeSoftware/EmissorCertificadosBackend/config/JwtAuthFilter.java
@@ -41,5 +41,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             SecurityContextHolder.clearContext();
             throw e;
         }
+
+        filterChain.doFilter( httpServletRequest, httpServletResponse );
+
     }
 }
